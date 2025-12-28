0d6c0cecc5d50dc2134bf05d2c0ca85d39873a47
Fakihi-Yasin <yasinfakihi21@gmail.com>
2025-12-28 05:41:32 +0100

    start implement  get next linefunctions

--- get_next_line.c
+++ get_next_line.c
@@ -9,4 +9,20 @@
 /*   Updated: 2025/12/27 15:44:55 by yafakihi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "get_next_line.h"
 
+char	*get_next_line(int fd)
+{
+    static char	*buffer;
+    char		*line;
+
+    if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, NULL, 0) < 0)
+        return (NULL);
+    line = NULL;
+    if (buffer)
+    {
+        line = ft_strjoin(line, buffer);
+        if (ft_strchr(buffer, '\n'))
+            return (line);
+    }
+}
--- get_next_line.h
+++ get_next_line.h
@@ -12,6 +12,9 @@
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+# ifndef BUFFER_SIZE
+# define BUFFER_SIZE 10
+# endif
 # include <stdlib.h>
 # include <fcntl.h>
 # include <stdio.h>
@@ -24,3 +27,5 @@ char	*ft_strchr(char *s, int c);
 size_t	ft_strlen(char *s);
 char	*ft_strdup(char *s);
 char	*ft_substr(char *s, unsigned int start, size_t len);
+
+#endif
