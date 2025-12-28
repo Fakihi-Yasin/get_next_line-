9b28d7489a24d84e15d93122e74a72d7e77b8444
Fakihi-Yasin <yasinfakihi21@gmail.com>
2025-12-28 03:43:16 +0100

    initial commit

---
+++ get_next_line.h
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yafakihi <yafakihi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/27 15:44:57 by yafakihi          #+#    #+#             */
+/*   Updated: 2025/12/27 15:55:32 by yafakihi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdlib.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include <unistd.h>
